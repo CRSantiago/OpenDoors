ca3d24326e2a27372d2ffadf8527386488403bff
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2024-03-11 20:42:05 -0400

    Improvements to UI

---
+++ client/src/features/ContributionsPage/ContributionCard.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const ContributionCard = ({
+  title,
+  description,
+  buttonText,
+  onClickHandler,
+}) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mr-5 my-10">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{description}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={onClickHandler}
+        >
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  )
+}
+export default ContributionCard
--- client/src/features/ContributionsPage/ContributionsPage.jsx
+++ client/src/features/ContributionsPage/ContributionsPage.jsx
@@ -1,14 +1,40 @@
 import React from "react"
-
+import ContributionCard from "./ContributionCard"
 const Contributions = () => {
   return (
-    <div>
-      <h1>Dsecribe how users can contribute</h1>
-      <p>
-        If you have any feedback on how we can improve the app, please reach
-        out!
-      </p>
-    </div>
+    <section className="flex items-center flex-col bg-gray-100">
+      <div className="flex items-center flex-col">
+        <h2 className="text-3xl md:text-5xl font-bold text-gray-800 my-3">
+          Contribute to Our Project
+        </h2>
+        <p>
+          We're always looking for ways to improve and would love your input and
+          contributions. Here's how you can help:
+        </p>
+      </div>
+      <div className="flex justify-">
+        <ContributionCard
+          title="Suggest Improvements"
+          description="Got an idea to make our project better? We'd love to hear your suggestions."
+          buttonText="Share Ideas"
+          onClickHandler={() => {
+            /* function to handle click event for improvements */
+          }}
+        />
+        <ContributionCard
+          title="How to Contribute"
+          description="Interested in contributing directly to the project? Check out our Github."
+          buttonText="Learn More"
+          onClickHandler={() => {
+            window.open(
+              "https://github.com/CRSantiago/OpenDoors",
+              "_blank",
+              "noopener,noreferrer"
+            )
+          }}
+        />
+      </div>
+    </section>
   )
 }
 
