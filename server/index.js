9578571c7d36e3ec64c0fc0e7a393efbababd899
Chris Santiago <chrisantiago217@gmail.com>
2024-03-24 13:26:25 -0400

    Code clean up

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 // server/index.js
-import express from "express"
-import helmet from "helmet"
-import cors from "cors"
-import swaggerJsdoc from "swagger-jsdoc"
-import swaggerUi from "swagger-ui-express"
-import "./database.js"
-import jobApplicationRoutes from "./routes/jobApplications.js"
-import userRoutes from "./routes/users.js"
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import './database.js'
+import jobApplicationRoutes from './routes/jobApplications.js'
+import userRoutes from './routes/users.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -14,24 +14,34 @@ const PORT = process.env.PORT || 3001
 // helmet middleware to secure the app by setting various HTTP headers
 app.use(helmet())
 app.use(cors())
+app.use(express.json()) // Middleware to parse JSON bodies
+
 const swaggerOptions = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "OpenDoors API",
-      version: "1.0.0",
-      description: "OpenDoors API Documentation",
+      title: 'OpenDoors API',
+      version: '1.0.0',
+      description: 'OpenDoors API Documentation',
     },
   },
-  apis: ["./docs/*.yaml"], // Path to the API docs
+  apis: ['./docs/*.yaml'], // Path to the API docs
 }
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-app.get("/", (req, res) => res.send("Hello World from OpenDoors Server!"))
-app.use(express.json()) // Middleware to parse JSON bodies
-app.use("/api/job-applications", jobApplicationRoutes)
-app.use("/api/users", userRoutes)
+
+// Serve static files from the React app only in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
+}
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api/job-applications', jobApplicationRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
