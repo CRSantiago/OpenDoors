9db071d0fa5fd3c0d0c4e227cffe6df0ad600199
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2024-04-17 13:05:47 -0400

    test get all applications error

--- client/src/features/Dashboard/api/getAllJobApplications.js
+++ client/src/features/Dashboard/api/getAllJobApplications.js
@@ -2,14 +2,15 @@ const getAllJobApplications = async () => {
   const user = JSON.parse(localStorage.getItem("authState")).user
   const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3001"
   console.log(apiUrl)
-  const reponse = await fetch(`${apiUrl}/api/job-applications`, {
+  const response = await fetch(`${apiUrl}/api/job-applications`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${user.token}`,
     },
   })
-  const data = await reponse.json()
+  console.log(response)
+  const data = await response.json()
   return data
 }
 
